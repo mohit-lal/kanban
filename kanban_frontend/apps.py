5fdff8c2bbdd8785297c8fbf9efd687f06a58fe3
mzitx <mohit.lal@javra.com>
2020-09-06 23:31:53 +0545

    template setup

---
+++ kanban_frontend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KanbanFrontendConfig(AppConfig):
+    name = 'kanban_frontend'
--- kanbansite/settings.py
+++ kanbansite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'api',
+    'kanban_frontend'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'kanbansite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
